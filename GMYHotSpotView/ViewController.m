53cbb929daca52543555d351d445e64b5b059f12
778477 <1032419360@qq.com>
2015-07-26 21:51:26 +0800

    add hotspotView use BetterLayout

--- GMYHotSpotView/ViewController.m
+++ GMYHotSpotView/ViewController.m
@@ -9,22 +9,18 @@
 #import "ViewController.h"
 #import "GMYHotSpotView.h"
 #import "GMYHotSpotViewNormalLayout.h"
+#import "GMYHotSpotViewBetterLayout.h"
 #import "GMYHotSpotModel.h"
 #import "UIDevice+iOSVersion.h"
+#import "UIView+EX.h"
 @interface ViewController ()
 @property (nonatomic, strong) GMYHotSpotView *hotspotView;
+@property (nonatomic, strong) GMYHotSpotView *hotspotView1;
 @end
 
 @implementation ViewController
 #pragma mark -
 #pragma mark - life cycle
-- (instancetype)init{
-    if(self = [super init]){
-        
-    }
-    return self;
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
@@ -32,26 +28,39 @@
      *  https://top.taobao.com/index.php?spm=a1z5i.1.2.1.hUTg2J&topId=HOME
      */
     NSArray *arr = @[@"立体涂鸦笔",@"紫外线牙刷消毒器",@"茶桌椅组合",@"雨伞",@"大圣归来公仔",@"套装女夏装2015",@"苹果6手机壳",@"秘密花园",@"沙滩裙",@"跑步机",@"皮鞋",@"水杯",@"凉鞋女",@"夹烟机 钓烟机",@"婚纱",@"iphone6plus手机壳",@"花盆",@"茶具",@"背带裙",@"对讲机"];
+    
+
     NSArray *formatedArr = [self formatHotspot:arr];
     
     [self.hotspotView updateHotSpotWithArray:formatedArr ClickHandle:^(NSInteger index, NSString *title) {
         NSLog(@"%ld - %@",index,title);
     }];
-    
     [self.view addSubview:self.hotspotView];
+    
+    
+    [self.hotspotView1 updateHotSpotWithArray:formatedArr ClickHandle:^(NSInteger index, NSString *title) {
+        NSLog(@"%ld - %@",index,title);
+    }];
+    [self.view addSubview:self.hotspotView1];
 }
 
-#pragma mark -
+#pragma mark - lzay Load subView
 - (GMYHotSpotView *)hotspotView{
     if(!_hotspotView){
         CGFloat yOffset = [[UIDevice currentDevice] systemVersionGeraterThanOrEqualTo:@"7.0"] ? 20.f :0.f;
         _hotspotView = [[GMYHotSpotView alloc] initWithFrame:CGRectMake(5, yOffset, self.view.frame.size.width - 10, self.view.frame.size.height/2) hotspotViewLayout:[GMYHotSpotViewNormalLayout new]];
-        
-//        _hotspotView.backgroundColor = [UIColor blueColor];
     }
     return _hotspotView;
 }
 
+- (GMYHotSpotView *)hotspotView1{
+    if(!_hotspotView1){
+        _hotspotView1 = [[GMYHotSpotView alloc] initWithFrame:CGRectMake(5, self.hotspotView.bottom + 20, self.view.frame.size.width - 10, self.view.frame.size.height/2) hotspotViewLayout:[GMYHotSpotViewBetterLayout new]];
+    }
+    return _hotspotView1;
+}
+
+#pragma mark - Model Format
 - (NSArray *)formatHotspot:(NSArray *)arr{
     NSMutableArray *models = [[NSMutableArray alloc] initWithCapacity:arr.count];
     [arr enumerateObjectsUsingBlock:^(NSString* obj, NSUInteger idx, BOOL *stop) {
