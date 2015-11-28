dfb658262e88534817ebac86ab3d34fca0c17a8b
778477 <1032419360@qq.com>
2015-11-28 17:15:17 +0800

    add GMYHotSpotViewDefines
    add New file

---
+++ GMYHotSpotView/Model/GMYHotSpotViewDefines.h
@@ -0,0 +1,14 @@
+//
+//  GMYHotSpotViewDefines.h
+//  Pods
+//
+//  Created by 郭妙友 on 15/11/28.
+//
+//
+
+#ifndef GMYHotSpotViewDefines_h
+#define GMYHotSpotViewDefines_h
+
+#define kGMYHotSpotViewTagBaseIndex 101
+
+#endif /* GMYHotSpotViewDefines_h */
