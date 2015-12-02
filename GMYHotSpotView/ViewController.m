72146ca026993c0c3fe28530deaad3c968e69947
778477 <1032419360@qq.com>
2015-12-02 22:42:12 +0800

    add title label

--- GMYHotSpotView/ViewController.m
+++ GMYHotSpotView/ViewController.m
@@ -29,8 +29,6 @@
      */
     NSArray *arr = @[@"立体涂鸦笔",@"紫外线牙刷消毒器",@"茶桌椅组合",@"雨伞",@"大圣归来公仔",@"套装女夏装2015",@"苹果6手机壳",@"秘密花园",@"沙滩裙",@"跑步机",@"皮鞋",@"水杯",@"凉鞋女",@"夹烟机 钓烟机",@"婚纱",@"iphone6plus手机壳",@"花盆",@"茶具",@"背带裙",@"对讲机"];
     
-
-    
     NSArray *formatedArr = [self formatHotspot:arr];
     [self.hotspotView updateHotSpotWithArray:formatedArr ClickHandle:^(NSInteger index, NSString *title) {
         NSLog(@"%ld - %@",index,title);
@@ -62,7 +60,10 @@
 - (GMYHotSpotView *)hotspotView{
     if(!_hotspotView){
         CGFloat yOffset = [[UIDevice currentDevice] systemVersionGeraterThanOrEqualTo:@"7.0"] ? 20.f :0.f;
-        _hotspotView = [[GMYHotSpotView alloc] initWithFrame:CGRectMake(5, yOffset, self.view.width - 10, self.view.height/2)
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, yOffset, self.view.width, 20.f)];
+        label.text = @"NormalLayout : ";
+        [self.view addSubview:label];
+        _hotspotView = [[GMYHotSpotView alloc] initWithFrame:CGRectMake(5, label.bottom, self.view.width - 10, self.view.height/2)
                                            hotspotViewLayout:[GMYHotSpotViewNormalLayout new]];
     }
     return _hotspotView;
@@ -70,10 +71,11 @@
 
 - (GMYHotSpotView *)hotspotView1{
     if(!_hotspotView1){
-        _hotspotView1 = [[GMYHotSpotView alloc] initWithFrame:CGRectMake(5, self.hotspotView.bottom + 20, self.view.width - 10, self.view.height/2)
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, self.hotspotView.bottom + 20, self.view.width, 20.f)];
+        label.text = @"BetterLayout : ";
+        [self.view addSubview:label];
+        _hotspotView1 = [[GMYHotSpotView alloc] initWithFrame:CGRectMake(5, label.bottom, self.view.width - 10, self.view.height/2)
                                             hotspotViewLayout:[GMYHotSpotViewBetterLayout new]];
-        
-        _hotspotView1.backgroundColor = [UIColor blackColor];
     }
     return _hotspotView1;
 }
